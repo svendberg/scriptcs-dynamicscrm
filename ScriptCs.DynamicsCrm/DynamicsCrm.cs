d6af7c9538cddc1d5487e15baea21e5e6c99c7f5
Harald Svendberg <harald@svendberg.net>
2013-06-17 10:51:01 +0200

    first commit

---
+++ ScriptCs.DynamicsCrm/DynamicsCrm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.ServiceModel.Description;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using ScriptCs.Contracts;
+
+namespace ScriptCs.DynamicsCrm
+{
+    public class DynamicsCrm : IScriptPackContext
+    {
+       public IOrganizationService GetOrganizationService(string server, string organization)
+       {
+          var clientCreds = new ClientCredentials();
+          var orgConfigInfo = ServiceConfigurationFactory.CreateConfiguration<IOrganizationService>(new Uri(server + "/" + organization + "/XRMServices/2011/Organization.svc"));
+          var orgserv = new OrganizationServiceProxy(orgConfigInfo, clientCreds);
+          orgserv.ServiceConfiguration.CurrentServiceEndpoint.Behaviors.Add(new ProxyTypesBehavior());
+          return orgserv;
+       }
+    }
+}
---
+++ ScriptCs.DynamicsCrm/ScriptPack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ScriptCs.Contracts;
+
+namespace ScriptCs.DynamicsCrm
+{
+   public class ScriptPack : IScriptPack
+   {
+      public void Initialize(IScriptPackSession session)
+      {
+         var namespaces = new[]
+            {
+               "Microsoft.Xrm.Sdk.Client",
+               "Microsoft.Xrm.Sdk",
+               "Microsoft.Xrm.Sdk.Query",
+               "Microsoft.Xrm.Sdk.Messages",
+            }.ToList();
+         namespaces.ForEach(session.ImportNamespace);
+
+         session.AddReference("System.Runtime.Serialization");
+      }
+
+      public IScriptPackContext GetContext()
+      {
+         return new DynamicsCrm();
+      }
+
+      public void Terminate()
+      {
+      }
+   }
+}
