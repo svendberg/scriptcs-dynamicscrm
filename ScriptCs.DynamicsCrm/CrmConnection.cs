b8650191fc6dde76f6e2639f8d9cfc398d1f4fc4
svendberg <harald@svendberg.net>
2013-06-17 19:28:32 +0200

    Using auth code from SDK to support multiple hosting enviroments

---
+++ ScriptCs.DynamicsCrm/CrmConnection.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.ServiceModel.Description;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Discovery;
+// These namespaces are found in the Microsoft.Xrm.Sdk.dll assembly
+// located in the SDK\bin folder of the SDK download.
+
+namespace ScriptCs.DynamicsCrm
+{
+    public class CrmConnection
+    {
+        // To get discovery service address and organization unique name, 
+        // Sign in to your CRM org and click Settings, Customization, Developer Resources.
+        // On Developer Resource page, find the discovery service address under Service Endpoints and organization unique name under Your Organization Information.
+        private readonly string _discoveryServiceAddress; 
+        private readonly string _organizationUniqueName; 
+        private readonly string _userName; 
+        private readonly string _password; 
+
+        // Provide domain name for the On-Premises org.
+        private readonly string _domain;
+
+        public CrmConnection(string discoveryServiceAddress, string organizationUniqueName, string username = null, string password = null, string domain = null)
+        {
+            _discoveryServiceAddress = discoveryServiceAddress;
+            _organizationUniqueName = organizationUniqueName;
+            _userName = username;
+            _password = password;
+            _domain = domain;
+        }
+
+
+        public IOrganizationService GetOrganizationService()
+        {
+            IServiceManagement<IDiscoveryService> serviceManagement = ServiceConfigurationFactory.CreateManagement<IDiscoveryService>(new Uri(_discoveryServiceAddress));
+            AuthenticationProviderType endpointType = serviceManagement.AuthenticationType;
+
+            // Set the credentials.
+            AuthenticationCredentials authCredentials = GetCredentials(endpointType);
+
+
+            String organizationUri = String.Empty;
+            // Get the discovery service proxy.
+            using (DiscoveryServiceProxy discoveryProxy = GetProxy<IDiscoveryService, DiscoveryServiceProxy>(serviceManagement, authCredentials))
+            {
+                // Obtain organization information from the Discovery service. 
+                if (discoveryProxy != null)
+                {
+                    // Obtain information about the organizations that the system user belongs to.
+                    OrganizationDetailCollection orgs = DiscoverOrganizations(discoveryProxy);
+                    // Obtains the Web address (Uri) of the target organization.
+                    organizationUri = FindOrganization(_organizationUniqueName,
+                        orgs.ToArray()).Endpoints[EndpointType.OrganizationService];
+                }
+            }
+
+            if (!String.IsNullOrWhiteSpace(organizationUri))
+            {
+                IServiceManagement<IOrganizationService> orgServiceManagement =ServiceConfigurationFactory.CreateManagement<IOrganizationService>(new Uri(organizationUri));
+
+                // Set the credentials.
+                AuthenticationCredentials credentials = GetCredentials(endpointType);
+
+                // Get the organization service proxy.
+                OrganizationServiceProxy organizationProxy = GetProxy<IOrganizationService, OrganizationServiceProxy>(orgServiceManagement, credentials);
+                // This statement is required to enable early-bound type support.
+                organizationProxy.EnableProxyTypes();
+                return organizationProxy;
+            }
+            throw new Exception("Could not get service");
+        }
+
+        /// <summary>
+        /// Obtain the AuthenticationCredentials based on AuthenticationProviderType.
+        /// </summary>
+        /// <param name="endpointType">An AuthenticationProviderType of the CRM environment.</param>
+        /// <returns>Get filled credentials.</returns>
+        private AuthenticationCredentials GetCredentials(AuthenticationProviderType endpointType)
+        {
+
+            AuthenticationCredentials authCredentials = new AuthenticationCredentials();
+            switch (endpointType)
+            {
+                case AuthenticationProviderType.ActiveDirectory:
+                    authCredentials.ClientCredentials.Windows.ClientCredential =
+                        new System.Net.NetworkCredential(_userName,
+                            _password,
+                            _domain);
+                    break;
+                case AuthenticationProviderType.LiveId:
+                    authCredentials.ClientCredentials.UserName.UserName = _userName;
+                    authCredentials.ClientCredentials.UserName.Password = _password;
+                    authCredentials.SupportingCredentials = new AuthenticationCredentials();
+                    authCredentials.SupportingCredentials.ClientCredentials =
+                        Microsoft.Crm.Services.Utility.DeviceIdManager.LoadOrRegisterDevice();
+                    break;
+                default: // For Federated and OnlineFederated environments.                    
+                    authCredentials.ClientCredentials.UserName.UserName = _userName;
+                    authCredentials.ClientCredentials.UserName.Password = _password;
+                    // For OnlineFederated single-sign on, you could just use current UserPrincipalName instead of passing user name and password.
+                    // authCredentials.UserPrincipalName = UserPrincipal.Current.UserPrincipalName;  //Windows/Kerberos
+                    break;
+            }
+
+            return authCredentials;
+        }
+
+        /// <summary>
+        /// Discovers the organizations that the calling user belongs to.
+        /// </summary>
+        /// <param name="service">A Discovery service proxy instance.</param>
+        /// <returns>Array containing detailed information on each organization that 
+        /// the user belongs to.</returns>
+        public OrganizationDetailCollection DiscoverOrganizations(
+            IDiscoveryService service)
+        {
+            if (service == null) throw new ArgumentNullException("service");
+            RetrieveOrganizationsRequest orgRequest = new RetrieveOrganizationsRequest();
+            RetrieveOrganizationsResponse orgResponse =
+                (RetrieveOrganizationsResponse)service.Execute(orgRequest);
+
+            return orgResponse.Details;
+        }
+
+        /// <summary>
+        /// Finds a specific organization detail in the array of organization details
+        /// returned from the Discovery service.
+        /// </summary>
+        /// <param name="orgFriendlyName">The friendly name of the organization to find.</param>
+        /// <param name="orgDetails">Array of organization detail object returned from the discovery service.</param>
+        /// <returns>Organization details or null if the organization was not found.</returns>
+        /// <seealso cref="DiscoveryOrganizations"/>
+        public OrganizationDetail FindOrganization(string orgFriendlyName,
+            OrganizationDetail[] orgDetails)
+        {
+            if (String.IsNullOrWhiteSpace(orgFriendlyName))
+                throw new ArgumentNullException("orgFriendlyName");
+            if (orgDetails == null)
+                throw new ArgumentNullException("orgDetails");
+            OrganizationDetail orgDetail = null;
+
+            foreach (OrganizationDetail detail in orgDetails)
+            {
+                if (String.Compare(detail.FriendlyName, orgFriendlyName,
+                    StringComparison.InvariantCultureIgnoreCase) == 0)
+                {
+                    orgDetail = detail;
+                    break;
+                }
+            }
+            return orgDetail;
+        }
+
+        /// <summary>
+        /// Generic method to obtain discovery/organization service proxy instance.
+        /// </summary>
+        /// <typeparam name="TService">
+        /// Set IDiscoveryService or IOrganizationService type to request respective service proxy instance.
+        /// </typeparam>
+        /// <typeparam name="TProxy">
+        /// Set the return type to either DiscoveryServiceProxy or OrganizationServiceProxy type based on TService type.
+        /// </typeparam>
+        /// <param name="serviceManagement">An instance of IServiceManagement</param>
+        /// <param name="authCredentials">The user's Microsoft Dynamics CRM logon credentials.</param>
+        /// <returns></returns>
+        private TProxy GetProxy<TService, TProxy>(
+            IServiceManagement<TService> serviceManagement,
+            AuthenticationCredentials authCredentials)
+            where TService : class
+            where TProxy : ServiceProxy<TService>
+        {
+            Type classType = typeof(TProxy);
+
+            if (serviceManagement.AuthenticationType !=
+                AuthenticationProviderType.ActiveDirectory)
+            {
+                AuthenticationCredentials tokenCredentials =
+                    serviceManagement.Authenticate(authCredentials);
+                // Obtain discovery/organization service proxy for Federated, LiveId and OnlineFederated environments. 
+                // Instantiate a new class of type using the 2 parameter constructor of type IServiceManagement and SecurityTokenResponse.
+                return (TProxy)classType
+                    .GetConstructor(new Type[] { typeof(IServiceManagement<TService>), typeof(SecurityTokenResponse) })
+                    .Invoke(new object[] { serviceManagement, tokenCredentials.SecurityTokenResponse });
+            }
+
+            // Obtain discovery/organization service proxy for ActiveDirectory environment.
+            // Instantiate a new class of type using the 2 parameter constructor of type IServiceManagement and ClientCredentials.
+            return (TProxy)classType
+                .GetConstructor(new Type[] { typeof(IServiceManagement<TService>), typeof(ClientCredentials) })
+                .Invoke(new object[] { serviceManagement, authCredentials.ClientCredentials });
+        }
+    }
+}
--- ScriptCs.DynamicsCrm/DynamicsCrm.cs
+++ ScriptCs.DynamicsCrm/DynamicsCrm.cs
@@ -1,21 +1,27 @@
-﻿using System;
-using System.Diagnostics.Contracts;
-using System.ServiceModel.Description;
-using Microsoft.Xrm.Sdk;
-using Microsoft.Xrm.Sdk.Client;
+﻿using Microsoft.Xrm.Sdk;
 using ScriptCs.Contracts;
 
 namespace ScriptCs.DynamicsCrm
 {
     public class DynamicsCrm : IScriptPackContext
     {
-       public IOrganizationService GetOrganizationService(string server, string organization)
-       {
-          var clientCreds = new ClientCredentials();
-          var orgConfigInfo = ServiceConfigurationFactory.CreateConfiguration<IOrganizationService>(new Uri(server + "/" + organization + "/XRMServices/2011/Organization.svc"));
-          var orgserv = new OrganizationServiceProxy(orgConfigInfo, clientCreds);
-          orgserv.ServiceConfiguration.CurrentServiceEndpoint.Behaviors.Add(new ProxyTypesBehavior());
-          return orgserv;
-       }
+        public IOrganizationService GetOrganizationService(string discoveryService, string organization)
+        {
+            var app = new CrmConnection(discoveryService, organization);
+            return app.GetOrganizationService();
+        }
+
+        public IOrganizationService GetOrganizationService(string discoveryService, string organization, string username, string password)
+        {
+            var app = new CrmConnection(discoveryService, organization, username, password);
+            return app.GetOrganizationService();
+        }
+
+        public IOrganizationService GetOrganizationService(string discoveryService, string organization, string username, string password, string domain)
+        {
+            var app = new CrmConnection(discoveryService, organization, username, password, domain);
+            return app.GetOrganizationService();
+        }
     }
 }
+
