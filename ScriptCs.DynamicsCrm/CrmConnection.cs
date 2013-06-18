f0df5f2fe98e52a6c77bed2547c563d399de8e4a
svendberg <harald@svendberg.net>
2013-06-18 21:49:25 +0200

    Added retrievemultiple example

--- ScriptCs.DynamicsCrm/CrmConnection.cs
+++ ScriptCs.DynamicsCrm/CrmConnection.cs
@@ -3,8 +3,6 @@ using System.ServiceModel.Description;
 using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Client;
 using Microsoft.Xrm.Sdk.Discovery;
-// These namespaces are found in the Microsoft.Xrm.Sdk.dll assembly
-// located in the SDK\bin folder of the SDK download.
 
 namespace ScriptCs.DynamicsCrm
 {
@@ -71,11 +69,7 @@ namespace ScriptCs.DynamicsCrm
             throw new Exception("Could not get service");
         }
 
-        /// <summary>
-        /// Obtain the AuthenticationCredentials based on AuthenticationProviderType.
-        /// </summary>
-        /// <param name="endpointType">An AuthenticationProviderType of the CRM environment.</param>
-        /// <returns>Get filled credentials.</returns>
+
         private AuthenticationCredentials GetCredentials(AuthenticationProviderType endpointType)
         {
 
@@ -106,12 +100,6 @@ namespace ScriptCs.DynamicsCrm
             return authCredentials;
         }
 
-        /// <summary>
-        /// Discovers the organizations that the calling user belongs to.
-        /// </summary>
-        /// <param name="service">A Discovery service proxy instance.</param>
-        /// <returns>Array containing detailed information on each organization that 
-        /// the user belongs to.</returns>
         public OrganizationDetailCollection DiscoverOrganizations(
             IDiscoveryService service)
         {
@@ -123,13 +111,6 @@ namespace ScriptCs.DynamicsCrm
             return orgResponse.Details;
         }
 
-        /// <summary>
-        /// Finds a specific organization detail in the array of organization details
-        /// returned from the Discovery service.
-        /// </summary>
-        /// <param name="orgFriendlyName">The friendly name of the organization to find.</param>
-        /// <param name="orgDetails">Array of organization detail object returned from the discovery service.</param>
-        /// <returns>Organization details or null if the organization was not found.</returns>
         /// <seealso cref="DiscoveryOrganizations"/>
         public OrganizationDetail FindOrganization(string orgFriendlyName,
             OrganizationDetail[] orgDetails)
@@ -152,18 +133,7 @@ namespace ScriptCs.DynamicsCrm
             return orgDetail;
         }
 
-        /// <summary>
-        /// Generic method to obtain discovery/organization service proxy instance.
-        /// </summary>
-        /// <typeparam name="TService">
-        /// Set IDiscoveryService or IOrganizationService type to request respective service proxy instance.
-        /// </typeparam>
-        /// <typeparam name="TProxy">
-        /// Set the return type to either DiscoveryServiceProxy or OrganizationServiceProxy type based on TService type.
-        /// </typeparam>
-        /// <param name="serviceManagement">An instance of IServiceManagement</param>
-        /// <param name="authCredentials">The user's Microsoft Dynamics CRM logon credentials.</param>
-        /// <returns></returns>
+      
         private TProxy GetProxy<TService, TProxy>(
             IServiceManagement<TService> serviceManagement,
             AuthenticationCredentials authCredentials)
@@ -177,15 +147,10 @@ namespace ScriptCs.DynamicsCrm
             {
                 AuthenticationCredentials tokenCredentials =
                     serviceManagement.Authenticate(authCredentials);
-                // Obtain discovery/organization service proxy for Federated, LiveId and OnlineFederated environments. 
-                // Instantiate a new class of type using the 2 parameter constructor of type IServiceManagement and SecurityTokenResponse.
                 return (TProxy)classType
                     .GetConstructor(new Type[] { typeof(IServiceManagement<TService>), typeof(SecurityTokenResponse) })
                     .Invoke(new object[] { serviceManagement, tokenCredentials.SecurityTokenResponse });
             }
-
-            // Obtain discovery/organization service proxy for ActiveDirectory environment.
-            // Instantiate a new class of type using the 2 parameter constructor of type IServiceManagement and ClientCredentials.
             return (TProxy)classType
                 .GetConstructor(new Type[] { typeof(IServiceManagement<TService>), typeof(ClientCredentials) })
                 .Invoke(new object[] { serviceManagement, authCredentials.ClientCredentials });
---
+++ ScriptCs.DynamicsCrm/retrievemultiple.csx
@@ -0,0 +1,11 @@
+var crm = Require<DynamicsCrm>();
+var orgService = crm.GetOrganizationService("http://crm2:5555/XrmServices/2011/Discovery.svc", "company", "user", "password", "domain");
+QueryByAttribute querybyattribute = new QueryByAttribute("account");
+querybyattribute.ColumnSet = new ColumnSet("name");
+querybyattribute.Attributes.AddRange("address1_city");
+querybyattribute.Values.AddRange("Redmond");
+EntityCollection retrieved = orgService.RetrieveMultiple(querybyattribute);
+foreach(var account in retrieved.Entities)
+{
+	Console.WriteLine(account["name"]);
+}
