efd29901c0ecc5a9989e65f6bc16fb6df08fbe9a
Harald Svendberg <harald@svendberg.net>
2013-06-18 15:51:50 +0200

    Added nuspec

--- ScriptCs.DynamicsCrm/Properties/AssemblyInfo.cs
+++ ScriptCs.DynamicsCrm/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ScriptCs.DynamicsCrm")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("scriptcs script pack for Dynamics CRM 2011")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Harald Svendberg")]
 [assembly: AssemblyProduct("ScriptCs.DynamicsCrm")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright Harald Svendberg (harald@svendberg.net)")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
