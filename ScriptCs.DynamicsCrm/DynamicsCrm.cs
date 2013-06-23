eb4a092a3badce48cd2fe0eb45e645dce6267bc5
svendberg <harald@svendberg.net>
2013-06-23 16:10:49 +0200

    Recuced to one GetOrganizationService method

--- ScriptCs.DynamicsCrm/DynamicsCrm.cs
+++ ScriptCs.DynamicsCrm/DynamicsCrm.cs
@@ -5,19 +5,7 @@ namespace ScriptCs.DynamicsCrm
 {
     public class DynamicsCrm : IScriptPackContext
     {
-        public IOrganizationService GetOrganizationService(string discoveryService, string organization)
-        {
-            var app = new CrmConnection(discoveryService, organization);
-            return app.GetOrganizationService();
-        }
-
-        public IOrganizationService GetOrganizationService(string discoveryService, string organization, string username, string password)
-        {
-            var app = new CrmConnection(discoveryService, organization, username, password);
-            return app.GetOrganizationService();
-        }
-
-        public IOrganizationService GetOrganizationService(string discoveryService, string organization, string username, string password, string domain)
+        public IOrganizationService GetOrganizationService(string discoveryService, string organization, string username = null, string password = null, string domain = null)
         {
             var app = new CrmConnection(discoveryService, organization, username, password, domain);
             return app.GetOrganizationService();
